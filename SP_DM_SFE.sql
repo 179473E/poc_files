b7e6a9d13922f4f0dcbf6e9b4aecdc9b73ffd1fc
179473E <david.adenzon@jeffco.k12.co.us>
2023-05-25 13:06:18 -0600

    actual ddl

--- SP_DM_SFE.sql
+++ SP_DM_SFE.sql
@@ -1,5 +1,24 @@
----adding comment here
----adding more comments
----test change
-----test change 
-select * from dual;
+
+
+-- Description: sql to load DIM_TIME table from IBMS
+--
+-- Modifications
+-- Date         Modifier           Comment
+-- ------------ ------------------ ----------------------------------------------------
+-- 2023-05-25   Adenzon             Initial version
+--
+--************************************************************************************
+create or replace view RPOD_DATA_MART.STUDENT_BIO_INFO(
+	STUDENT_ID,
+	ADDRESS,
+	PHONE,
+	EMAIL
+) as 
+(
+  SELECT 
+	STUDENT_ID,
+	ADDRESS,
+	PHONE,
+	EMAIL
+    FROM  PROD_W.STUDNETS T WHERE RCRD_CURRENT_FLAG='Y'
+  );
